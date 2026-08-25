2857d9b2d871ba39538d554dbb52540f25dc83e3
Denys <denispirog878@gmail.com>
2026-08-25 12:46:57 +0200

    Fix: declare Retrofit and Gson explicitly

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 
     testImplementation(kotlin("test"))
